842f24b1838061925eab25f6fe61355bc5e5c174
kelekelio <cristal01@gmail.com>
2020-10-06 19:16:08 +0200

    Initial commit

---
+++ src/main/java/com/grzegorz/kursy/KursyApplication.java
@@ -0,0 +1,13 @@
+package com.grzegorz.kursy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursyApplication.class, args);
+	}
+
+}
